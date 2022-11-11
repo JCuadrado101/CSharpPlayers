effb15f09e3a0174bcadaa0cf4e8b96a2a98148a
jordancuadrado <jac59155@gmail.com>
2022-11-11 16:42:09 -0500

    Make a program with 5 console.writeline statements in it.

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// Challenge: What Comes Next
 Console.WriteLine("Complete the next lesson!");
+
+// Challenge: The Makings of a Programmer
+Console.WriteLine("This is");
+Console.WriteLine("this making");
+Console.WriteLine("of a ");
+Console.WriteLine("programmer");
+Console.WriteLine("!");
