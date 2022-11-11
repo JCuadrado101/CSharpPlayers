0a07c8a8c5fd83116145fb3a2da37fa0c518e702
jordancuadrado <jac59155@gmail.com>
2022-11-11 16:51:39 -0500

    Make a program that runs as shown above:

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -7,3 +7,12 @@ Console.WriteLine("this making");
 Console.WriteLine("of a ");
 Console.WriteLine("programmer");
 Console.WriteLine("!");
+
+// Challenge: Consolas and Telim
+
+string name;
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got bread.");
