081ee4d9b9ed851df1df9b4d395da7daf1ac1858
jordancuadrado <jac59155@gmail.com>
2022-11-12 12:38:12 -0500

    Getting difference using modulus

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -6,7 +6,18 @@ float baseOfTriangle = float.Parse(Console.ReadLine());
 Console.WriteLine("What is the height of the triangle? (Numbers only");
 float heightOfTriangle = float.Parse(Console.ReadLine());
 
-
 float area = (baseOfTriangle + heightOfTriangle) / 2;
-
 Console.WriteLine("The area of the triangle is: " + area);
+
+
+// Challenge: The Four Sisters and the Duckbear
+
+int numOfSisters = 4;
+
+Console.WriteLine("How many eggs did the chickens lay?");
+int numOfChocolateEggs = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The sisters get " + numOfChocolateEggs / numOfSisters + " each");
+Console.WriteLine("The Duckbear gets " + numOfChocolateEggs % numOfSisters);
+
+
