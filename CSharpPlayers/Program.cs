ce65be01211cbb43bb70fc342edd5062f1653a5b
jordancuadrado <jac59155@gmail.com>
2022-11-11 21:37:35 -0500

    Display 14 different types and console.writeline

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Challenge: The Variable Shop
 
-Console.WriteLine("Hello, World!");
+// Display 14 different variable types
+
+byte oneByte = 0;
+short twoShort = 123;
+ushort threeUShort = 123;
+int fourInt = 123;
+uint fiveUInt = 123423;
+long sixLong = 19293939292;
+ulong sevenULong = 19383928398302;
+char eightChar = '@';
+string nineString = "String";
+bool tenBool = true;
+double elevenFloat = 3839.393;
+decimal twelveDecimal = 8393.394939m;
+float thirteenFloat = 3949302.393f;
+sbyte fourteenSByte = 32;
+
+Console.WriteLine("Byte: " + oneByte);
+Console.WriteLine("SByte: " + fourteenSByte);
+Console.WriteLine("Short: " + twoShort);
+Console.WriteLine("UShort : " + threeUShort);
+Console.WriteLine("Int : " + fourInt);
+Console.WriteLine("UInt : " + fiveUInt);
+Console.WriteLine("Long : " + sixLong);
+Console.WriteLine("ULong : " + sevenULong);
+Console.WriteLine("Char : " + eightChar);
+Console.WriteLine("Bool : " + tenBool);
+Console.WriteLine("Float : " + elevenFloat);
+Console.WriteLine("Decimal : " + twelveDecimal);
+Console.WriteLine("Float : " + thirteenFloat);
+
