5fa07502626d3ac67ef2011e7eac461c01d96d8b
jordancuadrado <jac59155@gmail.com>
2022-11-11 20:27:35 -0500

    Refactor and comment code

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Challenge: The Thing Namer 3000
 
-Console.WriteLine("Hello, World!");
+/*
+ ORIGINAL CODE
+ 
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+string c = "Of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + "" + d + "!");
+
+*/
+
+string a;
+string b;
+
+Console.WriteLine("What kind of thing are we talking about?");
+a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+b = Console.ReadLine();
+
+Console.WriteLine("The " + b + " " + a + " of doom 3000!");
