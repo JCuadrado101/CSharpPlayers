2cb8fea97ebd22cb2b7c6c6ecbe34292ebb8a27a
jordancuadrado <jac59155@gmail.com>
2022-11-12 19:29:31 -0500

    Calculating barrier

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Challenge: The Defense of Consolas
 
-Console.WriteLine("Hello, World!");
+Console.Title = "Defense of Consolas";
+
+Console.Clear();
+Console.WriteLine("Where is the next attack?");
+
+Console.WriteLine("Target Row?");
+int targetRow = int.Parse(Console.ReadLine());
+Console.WriteLine("Target Column?");
+int targetColumn = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Deploy To:");
+
+Console.WriteLine($"({targetRow}, {targetColumn - 1})");
+Console.WriteLine($"({targetRow + -1}, {targetColumn })");
+Console.WriteLine($"({targetRow}, {targetColumn + 1})");
+Console.WriteLine($"({targetRow + 1}, {targetColumn})");
