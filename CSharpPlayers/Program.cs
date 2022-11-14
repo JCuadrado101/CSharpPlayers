973276131f8ca97625de5310aa8e3e5a5587afbc
jordancuadrado <jac59155@gmail.com>
2022-11-13 20:39:43 -0500

    uncommenting code.

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,30 +1,30 @@
 ﻿// Challenge: The Prototype
 
-// Console.WriteLine("User 1, enter a number between 0 and 100");
-// int numberToGuess = int.Parse(Console.ReadLine());
-//
-// Console.WriteLine("User 2, guess the number");
-// int guessedNumber = -1;
-//
-// while (numberToGuess != guessedNumber)
-// {
-//     guessedNumber = int.Parse(Console.ReadLine());
-//     
-//     if (numberToGuess < guessedNumber)
-//     {
-//         Console.WriteLine($"{guessedNumber} is too high.");
-//         Console.Write("What is your next guess? ");
-//     }
-//     else if (numberToGuess > guessedNumber)
-//     {
-//         Console.WriteLine($"{guessedNumber} is too low.");
-//         Console.Write("What is your next guess? ");
-//     }
-//     if (numberToGuess == guessedNumber)
-//     {
-//         Console.WriteLine("You guessed the number!");
-//     }
-// }
+Console.WriteLine("User 1, enter a number between 0 and 100");
+int numberToGuess = int.Parse(Console.ReadLine());
+
+Console.WriteLine("User 2, guess the number");
+int guessedNumber = -1;
+
+while (numberToGuess != guessedNumber)
+{
+    guessedNumber = int.Parse(Console.ReadLine());
+    
+    if (numberToGuess < guessedNumber)
+    {
+        Console.WriteLine($"{guessedNumber} is too high.");
+        Console.Write("What is your next guess? ");
+    }
+    else if (numberToGuess > guessedNumber)
+    {
+        Console.WriteLine($"{guessedNumber} is too low.");
+        Console.Write("What is your next guess? ");
+    }
+    if (numberToGuess == guessedNumber)
+    {
+        Console.WriteLine("You guessed the number!");
+    }
+}
 
 
 // Challenge: The Magic Cannon
