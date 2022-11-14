f7d7c09d29aa888b80f3cd58307647c300107417
jordancuadrado <jac59155@gmail.com>
2022-11-14 18:46:25 -0500

    Got total and average of original array.

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -2,6 +2,9 @@
 
 int[] original = new int[5];
 int[] newArray = new int[5];
+int total = 0;
+float average = 0.0f;
+
 Console.WriteLine("Enter 5 numbers for a new array");
 
 for (int i = 0; i < newArray.Length; i++)
@@ -18,3 +21,14 @@ for (int i = 0; i < original.Length; i++)
 }
 
 Console.WriteLine(" Original Array: [{0}]", string.Join(", ", original));
+
+
+// The Laws of Freach
+
+foreach (int item in original)
+{
+    total += item;
+}
+
+Console.WriteLine("total: " + total);
+Console.WriteLine("Average: " + total/original.Length);
