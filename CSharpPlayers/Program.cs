f6dbdf6446774ac7809a3eaf87fddb19f3f0f060
jordancuadrado <jac59155@gmail.com>
2022-11-14 18:35:48 -0500

    creating array and duplicating array.

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Challenge: The Replicator of D'To
 
-Console.WriteLine("Hello, World!");
+int[] original = new int[5];
+int[] newArray = new int[5];
+Console.WriteLine("Enter 5 numbers for a new array");
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("New Array: [{0}]", string.Join(", ", newArray));
+Console.WriteLine(" Original Array:  [{0}]", string.Join(", ", original));
+
+for (int i = 0; i < original.Length; i++)
+{
+    original[i] = newArray[i];
+}
+
+Console.WriteLine(" Original Array: [{0}]", string.Join(", ", original));
