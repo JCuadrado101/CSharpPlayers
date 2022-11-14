b214ae4e3b779f05f7cffc51db8a9d96de3a86f6
jordancuadrado <jac59155@gmail.com>
2022-11-13 20:39:21 -0500

    Looping with modulus and colors.

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,29 +1,49 @@
 ﻿// Challenge: The Prototype
 
-Console.WriteLine("User 1, enter a number between 0 and 100");
-int numberToGuess = int.Parse(Console.ReadLine());
+// Console.WriteLine("User 1, enter a number between 0 and 100");
+// int numberToGuess = int.Parse(Console.ReadLine());
+//
+// Console.WriteLine("User 2, guess the number");
+// int guessedNumber = -1;
+//
+// while (numberToGuess != guessedNumber)
+// {
+//     guessedNumber = int.Parse(Console.ReadLine());
+//     
+//     if (numberToGuess < guessedNumber)
+//     {
+//         Console.WriteLine($"{guessedNumber} is too high.");
+//         Console.Write("What is your next guess? ");
+//     }
+//     else if (numberToGuess > guessedNumber)
+//     {
+//         Console.WriteLine($"{guessedNumber} is too low.");
+//         Console.Write("What is your next guess? ");
+//     }
+//     if (numberToGuess == guessedNumber)
+//     {
+//         Console.WriteLine("You guessed the number!");
+//     }
+// }
 
-Console.WriteLine("User 2, guess the number");
-int guessedNumber = -1;
 
-while (numberToGuess != guessedNumber)
+// Challenge: The Magic Cannon
+
+for (var i = 1; i < 100; i++)
 {
-    guessedNumber = int.Parse(Console.ReadLine());
-    
-    if (numberToGuess < guessedNumber)
+    if (i % 3 == 0)
     {
-        Console.WriteLine($"{guessedNumber} is too high.");
-        Console.Write("What is your next guess? ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
     }
-    else if (numberToGuess > guessedNumber)
+    else if (i % 5 == 0)
     {
-        Console.WriteLine($"{guessedNumber} is too low.");
-        Console.Write("What is your next guess? ");
-
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Electric");
     }
-
-    if (numberToGuess == guessedNumber)
+    else
     {
-        Console.WriteLine("You guessed the number!");
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine($"{i}: Normal");
     }
 }
