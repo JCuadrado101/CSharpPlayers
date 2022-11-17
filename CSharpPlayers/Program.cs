878bfba3c2bab4be989e99399d0b7735284a7fcf
jordancuadrado <jac59155@gmail.com>
2022-11-16 22:12:10 -0500

    enums

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Challenge: Simula's Test
 
-Console.WriteLine("Hello, World!");
+ChestState currentState = ChestState.Locked;
+
+while (true)
+{
+    Console.Write($"The chest is {currentState}. What would you like to do? ");
+    string input = Console.ReadLine();
+
+    if (currentState == ChestState.Locked && input == "unlock") currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "open") currentState = ChestState.Open;
+    if (currentState == ChestState.Open && input == "close") currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "lock") currentState = ChestState.Locked;
+
+
+}
+
+
+enum ChestState {Open, Closed, Locked};
