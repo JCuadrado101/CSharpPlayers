b9db1bff2cdd11bcdbc2c3c2055737b73eed0001
jordancuadrado <jac59155@gmail.com>
2022-11-23 23:32:03 -0500

    classes

--- CSharpPlayers/Program.cs
+++ CSharpPlayers/Program.cs
@@ -1,3 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Challenge: Vin Fletcher's Arrows
 
-Console.WriteLine("Hello, World!");
+class Arrow
+{
+    public Arrowhead _arrowhead;
+    public Fletching _fletching;
+    public float _length;
+    
+    public Arrow(Arrowhead arrowhead, Fletching fletching, float length)
+    {
+        _arrowhead = arrowhead;
+        _fletching = fletching;
+        _length = length;
+    }
+    
+    Arrowhead GetArrowheadType()
+    {
+        Console.WriteLine("Arrowhead type (steel, wood, obsidian)");
+        string input = Console.ReadLine();
+        return input switch
+        {
+            "steel" => Arrowhead.Steel,
+            "wood" => Arrowhead.Wood,
+            "obsidian" => Arrowhead.Obsidian
+        };
+    }
+
+    Fletching GetFletchingType()
+    {
+        Console.WriteLine("Fletching type (plastic, turkey feather, goose feather)");
+        string input = Console.ReadLine();
+        return input switch
+        {
+            "plastic" => Fletching.Plastic,
+            "turkey feather" => Fletching.TurkeyFeathers,
+            "goose feather" => Fletching.GooseFeathers
+        };
+    }
+
+    float GetLength()
+    {
+        float length = 0;
+        while (length < 60 || length > 100)
+        {
+            Console.Write("Arrow length (between 60 and 100)");
+            length = Convert.ToSingle(Console.ReadLine());
+        }
+        return length;
+    }
+    
+    public float GetCost()
+    {
+        float arrowheadCost = _arrowhead switch
+        {
+            Arrowhead.Steel => 10,
+            Arrowhead.Wood => 3,
+            Arrowhead.Obsidian => 5
+        };
+
+        float fletchingCost = _fletching switch
+        {
+            Fletching.Plastic => 10,
+            Fletching.TurkeyFeathers => 5,
+            Fletching.GooseFeathers => 3
+        };
+
+        float shaftCost = 0.05f * _length;
+
+        return arrowheadCost + fletchingCost + shaftCost;
+    }
+}
+
+
+
+enum Arrowhead { Steel, Wood, Obsidian }
+enum Fletching { Plastic, TurkeyFeathers, GooseFeathers }
